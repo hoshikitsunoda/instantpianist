025db8ba7b3e702ca8157d39b6b3a933d0c06ba3
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-14 13:24:26 -0700

    Removed code comments from main.js #4

--- main.js
+++ main.js
@@ -7,37 +7,8 @@ $sound1.addEventListener('click', function() {
   $sound1.play()
 }, false)
 
-/*
-
-$sound2.addEventListener('click', function() {
-  var $sound2 = new Audio('sounds/snare1.wav')
-  $sound2.play()
-}, false)
-
-var column = {
-  current: 0,
-  id: null
-}
-
-function play() {
-  var trs = document.getElementsByTagName("tr")
-  for(var i = 0; i < trs.length; i++){
-    if(trs.getElementsByTagName("td").className.matches('play')){
-      setInterval()
-    }
-  }
-}
-
-*/
-
 document.getElementById('pads').addEventListener('click', function(event){
   if(event.target && event.target.matches('button')) {
     event.target.classList.toggle('play')
   }
 })
-
-/*
-
-$start.addEventListener('click', play)
-
-*/
