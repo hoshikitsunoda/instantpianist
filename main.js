4729229583c2f2b0422206739a09079789f0ff35
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-15 15:25:29 -0700

    Make function that triggers the sequence by clicking start. #7

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 var $sound1 = document.querySelector('#sound1')
 var $sound2 = document.querySelector('#sound2')
-var $start = document.querySelector('#start')
+var $start = document.querySelector('#startbutton')
 
-$sound1.addEventListener('click', function() {
+function sound1() {
   var $sound1 = new Audio('sounds/kick1.wav')
   $sound1.play()
-}, false)
+}
+
+$sound1.addEventListener('click', sound1, false)
 
 document.getElementById('pads').addEventListener('click', function(event){
   if(event.target && event.target.matches('button')) {
     event.target.classList.toggle('play')
   }
 })
+
+var currentCol = 0
+var interval = 0
+var $buttons = document.querySelectorAll('button')
+
+function columnReader() {
+  var $button = $buttons[currentCol]
+  if ($button !== undefined) {
+    if ($button.classList.contains('play')) {
+      sound1()
+    }
+    if (currentCol === 15) {
+        currentCol = 0
+    } else {
+    currentCol++
+    }
+  }
+}
+
+function play() {
+  interval = setInterval(columnReader, 500)
+}
+
+$start.addEventListener('click', play, false)
