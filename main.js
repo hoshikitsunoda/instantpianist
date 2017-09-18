30fae4e8b8e2de3d52233a781c84344906eb605f
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-18 15:23:23 -0700

    The sequencer reads 3 rows and play back sounds. #8

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 var $sound1 = document.querySelector('#c-note')
-var $sound2 = document.querySelector('#sound2')
+var $sound2 = document.querySelector('#cs-note')
+var $sound3 = document.querySelector('#d-note')
 var $start = document.querySelector('#startbutton')
 var $stop = document.querySelector('#stopbutton')
+var sound1 = new Audio('sounds/kick1.wav')
+var sound2 = new Audio('sounds/snare1.wav')
+var sound3 = new Audio('sounds/hihat.wav')
 
-function sound1() {
-  var $sound1 = new Audio('sounds/kick1.wav')
-  $sound1.play()
-}
-
-$sound1.addEventListener('click', sound1, false)
+$sound1.addEventListener('click', function () {
+  sound1.play()
+}, false)
+$sound2.addEventListener('click', function () {
+  sound2.play()
+}, false)
+$sound3.addEventListener('click', function () {
+  sound3.play()
+}, false)
 
 document.getElementById('pads').addEventListener('click', function(event){
   if(event.target && event.target.matches('button')) {
@@ -22,16 +29,25 @@ var $buttons = document.querySelectorAll('button')
 var started = false
 
 function columnReader() {
-  var $button = $buttons[currentCol]
-  if ($button !== undefined) {
-    if ($button.classList.contains('play')) {
-      sound1()
+
+  for (var i = 0; i < $buttons.length; i++) {
+    var $row1 = $buttons[currentCol]
+    var $row2 = $buttons[currentCol + 16]
+    var $row3 = $buttons[currentCol + 32]
+    if ($row1 !== undefined && $row1.classList.contains('play')) {
+        sound1.play()
     }
-    if (currentCol === 15) {
-      currentCol = 0
-    } else {
-      currentCol++
+    if ($row2 !== undefined && $row2.classList.contains('play')) {
+        sound2.play()
     }
+    if ($row3 !== undefined && $row3.classList.contains('play')) {
+        sound3.play()
+    }
+  }
+  if (currentCol === 15) {
+    currentCol = 0
+  } else {
+    currentCol++
   }
 }
 
